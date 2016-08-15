4ff8da2a9dd51a979982753b67aee1c1706834fd
noodlze <modularo0o@gmail.com>
2016-08-15 19:56:18 +0700

    created 08/2016

---
+++ v2.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<string>
+#include<vector>
+#include<bitset>
+#include<map>
+#include<queue>
+#include<stack>
+#include<set>
+#include<cmath>
+#include<algorithm>
+#include<utility>
+#include<climits>
+#include<sstream>
+
+using namespace std;
+
+#define nl printf("\n")
+#define space printf(" ")
+
+#define mem(x,val) memset(x,val,sizeof(x))
+#define rite(x) freopen(x,"w",stdout)
+#define read(x) freopen(x,"r",stdin)
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main() {
+    read("in.txt");
+//rite("out.txt");
+    return 0;
+    }

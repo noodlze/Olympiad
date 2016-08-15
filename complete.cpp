7e84c942e51aec1dd550758512715614298a0aff
noodlze <modularo0o@gmail.com>
2016-08-15 19:49:11 +0700

    complete.cpp

---
+++ complete.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <utility>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+#define SCD(t) scanf("%d",&t)
+#define SCLD(t) scanf("%ld",&t)
+#define SCLLD(t) scanf("%lld",&t)
+#define SCC(t) scanf("%c",&t)
+#define SCS(t) scanf("%s",t)
+#define SCF(t) scanf("%f",&t)
+#define SCLF(t) scanf("%lf",&t)
+#define PRD(t) printf("%d",t)
+#define PRLD(t) printf("%ld",t)
+#define PRLLD(t) printf("%lld",t)
+#define PRC(t) printf("%c",t)
+#define PRS(t) printf("%s",t)
+#define PRF(t) printf("%f",t)
+#define PRLF(t) printf("%lf",t)
+#define nl printf("\n")
+#define space printf(" ")
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+
+
+#define mem(x,val) memset((x),(val),sizeof(x));
+#define rite(x) freopen(x,"w",stdout);
+#define read(x) freopen(x,"r",stdin);
+#define tl(x) (int)ceil(x) //round up to int
+#define tx(x) (int)floor(x) //round down to int
+
+#define MP make_pair
+#define pb push_back
+#define xx first
+#define yy second
+
+#define inf (1<<30)
+#define eps 1e-9
+#define pi 3.1415926535897932384626433832795
+#define mod 1000000007
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef string st;
+
+typedef vector<int> vi;
+typedef vector<st> vs;
+typedef map<int,int> mii;
+typedef map<st,int> msi;
+typedef set<int> si;
+typedef set<st> ss;
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+
+int main() {
+ 	read("in.txt");
+ 	//rite();
+    cout << (1 << 1);
+ 	return 0;
+	}
